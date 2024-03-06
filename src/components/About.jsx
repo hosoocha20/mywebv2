dbfbab0727a1148cbaed4d5524f2cea5f632fbf6
Hosoo Cha <hosoo.cha@gmail.com>
2024-03-06 22:41:02 +1300

    Update About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,6 +6,8 @@ import { SiTailwindcss, SiSass, SiNodedotjs } from "react-icons/si";
 import { TbApi } from "react-icons/tb";
 import { BsFiletypeSql, BsGit } from "react-icons/bs";
 import { BiLogoTypescript } from "react-icons/bi";
+import { FaReact } from "react-icons/fa";
+import { RiJavascriptFill } from "react-icons/ri";
 import Reveal from "./Reveal";
 import RevealFade from "./RevealFade";
 
@@ -19,39 +21,52 @@ const About = forwardRef((props, ref) => {
       <Reveal d={0}>
         <div className="md:text-clamp1 sm:text-clamp2 text-clamp3 ">
           <p className="   inline italic ">
-            I am an Entry Level{" "}
+            I am a{" "}
             <span className="text-clamp4  text-[#f8d61e] font-semibold  inline not-italic">
               WEB DEVELOPER
             </span>
+            {" "}driven by{" "}
+            <span className=" about-emphasis italic">innovation</span><span className="text-[#f5d008]">.</span>
           </p>
-          <p className=" leading-tight">
-            with a passion for building websites that provide{" "}
-            <span className=" about-emphasis italic">great user experience</span><span className="text-[#f5d008]">.</span>
-          </p>
+
         </div>
       </Reveal>
       <Reveal d={0.3}>
         <div className="md:text-clamp1 sm:text-clamp2 text-clamp3  text-center w-[95%] sm:w-[80%] md:w-[80%] mx-auto my-0">
           <p>
-            I am no expert in <span className="baskerville italic font-medium ">DESIGN</span> but I enjoy exploring{" "}
-            <span className=" about-emphasis italic">creativity</span> and{" "}
-            <span className=" about-emphasis italic">visual harmony</span> and <span className="baskerville italic">CRAFTING </span>
-             the elements to create <span className="baskerville italic font-medium">AESTHETIC</span> websites.
+          I'm passionate about crafting innovative digital solutions that not only enhance <span className="baskerville italic font-medium inline-block ">user experience</span> but also captivate{" "}
+            <span className=" about-emphasis italic">visually</span>.
           </p>
         </div>
       </Reveal>
       <Reveal d={0.6}>
         <div className=" md:text-clamp1 sm:text-clamp2 text-clamp3  text-right">
           <p>
-            I work primarily with <span className="text-[#2a3dfa] font-medium">REACT</span>{" "}
-            and <span className="text-[#2a3dfa] font-medium">JAVASCRIPT</span>.
+          I find immense satisfaction in blending <span className="text-[#2a3dfa] font-medium">FUNCTIONALITY</span>{" "}
+            with <span className="text-[#2a3dfa] font-medium">AESTHETICS</span>,
           </p>
-          <p>However, I am also knowledgeable in the following technologies:</p>
+          <p>and I'm constantly seeking creative ways to push the boundaries of web development.</p>
         </div>
       </Reveal>
 
         <RevealFade d={0.9}>
           <div className="mx-auto my-0 grid grid-cols-3   w-[95%] sm:w-[80%] md:w-[60%] gap-y-[1.5rem] gap-x-[10px] mt-[1rem]">
+            <div className="flex items-center justify-center gap-x-[0.3rem] ">
+              {" "}
+              <FaReact className="text-[1.5rem] text-[#61dbfb]" />
+              <p className="text-[12px] sm:text-[14px] md:text-base">REACT</p>
+            </div>
+            <div className="flex items-center justify-center gap-x-[0.3rem] ">
+              {" "}
+              <RiJavascriptFill  className="text-[1.5rem] "  color={"#f7df1e"}/>
+              <p className="text-[12px] sm:text-[14px] md:text-base">Javacript</p>
+            </div>
+            <div className="flex items-center justify-center gap-x-[0.3rem] ">
+              {" "}
+              <BiLogoTypescript  className="text-[1.5rem] text-[#3178C6]" />
+              <p className="text-[12px] sm:text-[14px] md:text-base">Typescript</p>
+            </div>
+
             <div className="flex items-center justify-center gap-x-[0.3rem] ">
               {" "}
               <AiFillHtml5 className="text-[1.5rem] text-[#e34c26]" />
@@ -68,11 +83,6 @@ const About = forwardRef((props, ref) => {
               {" "}
               <SiSass className="text-[1.5rem] text-[#c69]" />
               <p className="text-[12px] sm:text-[14px] md:text-base">SCSS</p>
-            </div>
-            <div className="flex items-center justify-center gap-x-[0.3rem] ">
-              {" "}
-              <BiLogoTypescript  className="text-[1.5rem] text-[#3178C6]" />
-              <p className="text-[12px] sm:text-[14px] md:text-base">Typescript</p>
             </div>
             <div className="flex items-center justify-center gap-x-[0.3rem] ">
               {" "}
